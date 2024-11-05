7bfa53e51fdb10ba2af2c0aeece3d159543aea17
hajn1e <thexhaj888@gmail.com>
2024-11-05 09:31:10 +0100

    aaa

--- metódusok/Program.cs
+++ metódusok/Program.cs
@@ -7,6 +7,17 @@
 
 			koszonto();
             Console.WriteLine(Osszeadas(10,5));
+			int a = 5;
+            Console.WriteLine(Dublaz(a));
+			int duplazott=Dublaz(a);
+            Console.WriteLine(a);
+            Console.WriteLine(duplazott);
+			Console.WriteLine(osszeg(10));
+            
+			int elso = 5;
+			int masodik = 10;
+			Csere(ref elso,ref masodik);
+			Console.WriteLine(elso+" " + masodik);
 			Console.ReadLine();
 		}
 
@@ -21,6 +32,34 @@
 			/*int osszeg = a + b;
 			return osszeg;*/
 			return a + b;
+		}
+
+		static int Dublaz(int szam)
+		{
+			return szam*2;
+		}
+
+		static string Dublaz()
+		{
+			return a;
+        }
+
+		static int Dublaz(int a, int b)
+		{
+			return a+b;
+		}
+		static int osszeg(int a , int b=5, int c=2)
+		{
+			return a + b+c; 
+		}
+
+		static void Csere(ref int a,ref int b)
+		{
+			int temp = a;
+			a=b; 
+			b=temp;
+			Console.WriteLine(a + " " + b);
+
 		}
 	}
 }
